29a941e72ddd5857e12c132ee7959e5f11e4f64f
jaspreet singh <js480292@gmail.com>
2026-01-23 12:21:40 +0530

    fixed

--- f2hfrontend/src/api/axios.js
+++ f2hfrontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logoutUser } from "../services/authService";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://f2hdjango.onrender.com/api/",
   withCredentials: true,
 });
 
